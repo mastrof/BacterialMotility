a7b17569252556b8702c2c1f9042ba8836024158
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-19 16:57:54 +0100

    Add some default parameters to internal state and define dummy function

--- src/types.jl
+++ src/types.jl
@@ -8,6 +8,9 @@ AbstractInternalState = AbstractDict{String}
 StateValue = Union{Real, Array}
 InternalState = Dict{String, StateValue}
 
+function dummy(x...)
+    ;
+end
 
 @doc raw"""
     AbstractBacterium{D,T,F<:Function,S<:AbstractInternalState}
@@ -30,22 +33,33 @@ Concrete type for bacterium with sensing capabilities and one (or multiple) inte
     - `sense!::F`: function defining sensing behavior
     - `state::S`: properties, geometry, internal state variables
 
-run!, turn! and sense! can, in principle, all depend AND act in-place on state.
 All arguments and parameters are optional.
 If parameters D and T are not given, they are assumed to be D=3 and T=Float64.
 If r and v are not defined, they are initialized to zeros(T,D).
-If run!, turn!, sense! are not defined, they are assigned to the identity function.
+If run!, turn!, sense! are not defined, they are assigned to a dummy function that does nothing.
+Some standard values (based on E.coli) are provided by default in state.
+
+Ideally, `run!` should take two parameters, bacterium (self) and an externally provided integration timestep;
+turn! should only take bacterium (self) as input; sense! should take bacterium and an instance of AbstractField.
 """
 @with_kw struct Bacterium{D,T,F,S} <: AbstractBacterium{D,T,F,S}
     id::String = "" # identifier
     r::MVector{D,T} = zeros(MVector{D,T}) # position vector
     v::MVector{D,T} = zeros(MVector{D,T})# velocity vector
-    run!::F = identity# displacement
-    turn!::F = identity# reorientation
-    sense!::F = identity# kinetic/tactic behavior
-    state::S = InternalState() # internal state variables
+    run!::F = dummy # displacement
+    turn!::F = dummy # reorientation
+    sense!::F = dummy # kinetic/tactic behavior
+    state::S = InternalState(
+        "Radius" => 0.5, # μm, typical value for E.coli
+        "ReorientationRate" => 1.0, # 1/s, typical value for E.coli
+        "RunState" => 1, # currently only used for run-reverse-flick motility; 1=reverse 0=flick
+        "PrevPosition" => r,
+        "PrevVelocity", => v,
+    )
 end # state
 
 Bacterium{D,T}(; kwargs...) where {D,T} = Bacterium{D,T,Function,InternalState}(; kwargs...)
 Bacterium{D}(; kwargs...) where D = Bacterium{D,Float64,Function,InternalState}(; kwargs...)
 Bacterium(; kwargs...) = Bacterium{3,Float64,Function,InternalState}(; kwargs...)
+
+
