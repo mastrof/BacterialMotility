d04d4430d55d992f0e7b858862f2720ae258db6b
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-19 17:05:36 +0100

    Define function for a single step of integration

---
+++ src/integration.jl
@@ -0,0 +1,24 @@
+@doc raw"""
+    step!(bacterium::B, dt, f::F) where {B<:AbstractBacterium,F<:AbstractField}
+    step!(bacteria::T, dt, f::F) where {B<:AbstractBacterium,T<:AbstractVector{B},F<:AbstractField}
+
+Perform a single integration step over the interval dt, composed as:
+    1) run
+    2) sensing
+    3) reorientation attempt
+"""
+function step!(bacterium::B, dt, f::F) where {B<:AbstractBacterium,F<:AbstractField}
+    bacterium.run!(bacterium, dt)
+    bacterium.sense!(bacterium, f)
+    if rand() < bacterium.state["ReorientationRate"] * dt
+        bacterium.turn!(bacterium)
+    end # if
+end # function
+
+
+function step!(bacteria::T, dt, f::F) where {B<:AbstractBacterium,T<:AbstractVector{B},F<:AbstractField}
+    for bacterium in bacteria
+        step!(bacterium, dt, f)
+    end # for
+end # function
+
