69632fbd6e5a570f6d52761cc1ff493f424cc3e6
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-19 16:59:49 +0100

    Define abstract field interface and two basic instances

---
+++ src/fields.jl
@@ -0,0 +1,15 @@
+abstract type AbstractField end
+
+@with_kw struct AnalyticField <: AbstractField
+    ϕ::Function
+    ∇ϕ::Function
+end # struct
+
+@with_kw struct Concentration_SteadyDiffusionSphericalSource_3D <: AbstractField
+    R::Float64 # μm, source radius
+    C::Float64 # μM, concentration at source surface
+    Cbg::Float64 = 0.0 # μM, background concentration
+    c = r -> Cbg + C*R/r # concentration field
+    ∇c = r -> -C*R/(r*r) # concentration gradient (radial towards center)
+end # struct
+    
