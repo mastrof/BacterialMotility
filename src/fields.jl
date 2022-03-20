8e1bd1d46dd855aa19239dd1c9484db08ba800c8
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-20 22:19:14 +0100

    Make minor edits to field interface

--- src/fields.jl
+++ src/fields.jl
@@ -1,5 +1,6 @@
 export
-    AbstractField, EmptyField, AnalyticField,
+    AbstractField, EmptyField,
+    AbstractAnalyticField, AnalyticField,
     Concentration_SteadyDiffusionSphericalSource_3D
 
 abstract type AbstractField end
@@ -8,16 +9,18 @@ struct EmptyField <: AbstractField
     ;
 end # struct
 
-@with_kw struct AnalyticField <: AbstractField
+abstract type AbstractAnalyticField <: AbstractField end
+
+@with_kw struct AnalyticField <: AbstractAnalyticField
     ϕ::Function
     ∇ϕ::Function
 end # struct
 
-@with_kw struct Concentration_SteadyDiffusionSphericalSource_3D <: AbstractField
+@with_kw struct Concentration_SteadyDiffusionSphericalSource_3D <: AbstractAnalyticField
     R::Float64 # μm, source radius
     C::Float64 # μM, concentration at source surface
     Cbg::Float64 = 0.0 # μM, background concentration
-    c = r -> Cbg + C*R/r # concentration field
-    ∇c = r -> -C*R/(r*r) # concentration gradient (radial towards center)
+    ϕ::Function = b -> Cbg + C*R/norm(b.r) # concentration field
+    ∇ϕ::Function = b -> -C*R/(norm(b.r)^2) # concentration gradient (radial towards center)
 end # struct
-    
+
