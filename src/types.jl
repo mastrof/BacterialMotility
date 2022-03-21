8161e4630444b85de6289796aac84e6386381182
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-21 17:58:57 +0100

    Simplify Bacterium interface with less strict typing

--- src/types.jl
+++ src/types.jl
@@ -19,28 +19,28 @@ function dummy(x...)
 end
 
 @doc raw"""
-    AbstractBacterium{D,T,F<:Function,S<:AbstractProperties}
+    AbstractBacterium{D,T}
 
 General interface for bacterium object.
     - `D`: dimensionality of the space in which the bacterium lives
     - `T`: type of the space in which the bacterium lives
 """
-abstract type AbstractBacterium{D,T,F<:Function,S<:AbstractProperties} end
+abstract type AbstractBacterium{D,T} end
 
 @doc raw"""
-    struct Bacterium{D,T,F,S} <: AbstractBacterium{D,T,F,S}
+    struct Bacterium{D,T} <: AbstractBacterium{D,T}
 
 Concrete type for bacterium with sensing capabilities and one (or multiple) internal state variable.
     - `id::String`: identifier (e.g. species, strain...)
     - `r::MVector{D,T}`: position vector
     - `v::MVector{D,T}`: velocity vector
-    - `run!::F`: function defining displacement, acts in place on r
-    - `turn!::F`: function defining reorientation, acts in place on v
-    - `sense!::F`: function defining sensing behavior
-    - `state::S`: properties, geometry, internal state variables
+    - `run!`: function defining displacement, acts in place on r
+    - `turn!`: function defining reorientation, acts in place on v
+    - `sense!`: function defining sensing behavior
+    - `state`: properties, geometry, internal state variables
 
 All arguments and parameters are optional.
-If parameters D and T are not given, they are assumed to be D=3 and T=Float64. F<:Function and S<:AbstractProperties.
+If parameters D and T are not given, they are assumed to be D=3 and T=Float64.
 If r and v are not defined, they are initialized to zeros(T,D).
 If run!, turn!, sense! are not defined, they are assigned to a dummy function that does nothing.
 Some standard properties are provided by default for state.
@@ -48,16 +48,15 @@ Some standard properties are provided by default for state.
 Ideally, `run!` should take two parameters, bacterium (self) and an externally provided integration timestep;
 turn! should only take bacterium (self) as input; sense! should take bacterium and an instance of AbstractField.
 """
-@with_kw struct Bacterium{D,T,F,S} <: AbstractBacterium{D,T,F,S}
+@with_kw struct Bacterium{D,T} <: AbstractBacterium{D,T}
     id::String = "" # identifier
     r::MVector{D,T} = zeros(MVector{D,T}) # position vector
     v::MVector{D,T} = zeros(MVector{D,T})# velocity vector
-    run!::F = dummy # displacement
-    turn!::F = dummy # reorientation
-    sense!::F = dummy # kinetic/tactic behavior
-    state::S = properties()
+    run! = dummy # displacement
+    turn! = dummy # reorientation
+    sense! = dummy # kinetic/tactic behavior
+    state = properties()
 end # state
 
-Bacterium{D,T}(; kwargs...) where {D,T} = Bacterium{D,T,Function,Properties}(; kwargs...)
-Bacterium{D}(; kwargs...) where D = Bacterium{D,Float64,Function,Properties}(; kwargs...)
-Bacterium(; kwargs...) = Bacterium{3,Float64,Function,Properties}(; kwargs...)
+Bacterium{D}(; kwargs...) where D = Bacterium{D,Float64}(; kwargs...)
+Bacterium(; kwargs...) = Bacterium{3,Float64}(; kwargs...)
