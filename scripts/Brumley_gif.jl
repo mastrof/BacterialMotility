dbcacadb4ba85f727d0e860db2c6f299475c4020
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-03-28 20:51:37 +0200

    Script for Brumley chemotaxis and animations for both run-tumble (no noise) and run-reverse-flick (with noise)

--- scripts/Brumley_gif.jl
+++ scripts/Brumley_gif.jl
@@ -12,7 +12,7 @@ function circleShape(h, k, r)
 end # function
 
 
-Lbox = 900 # μm
+Lbox = 1200 # μm
 d = 3 # dimensionality
 U = 30.0 # μm/s, speed, same for all species
 
@@ -24,7 +24,7 @@ function randvelocity(d,U)
 end # function
 randvelocity(U) = randvelocity(d,U)
 
-custom_tumble!(bacterium) = tumble!(bacterium, Degenerate(π/4))
+custom_tumble!(bacterium) = tumble!(bacterium, Gamma(9, 7.5))
 custom_revflick!(bacterium) = reverse_flick!(bacterium, Normal(π,π/8), Normal(π/2,π/8))
 
 
@@ -46,18 +46,18 @@ R = 30.0
 C = 2.0
 Cbg = 0.01
 f = Concentration_SteadyDiffusionSphericalSource_3D(R=R, C=C, Cbg=Cbg)
-s_chemo = propertiesBrumley("ChemotacticPrecision" => 0.0)
+s_chemo = propertiesBrumley("ChemotacticPrecision" => 1.0)
 s_nochemo = propertiesBrumley("ChemotacticPrecision" => 0.0, "ReceptorGain" => 0.0)
 
 species = ["Random Walk", "Chemotaxis"]
-pop1 = [BacteriumBrumley(id = species[1], r = randposition(), v = randvelocity(20.5), turn! = custom_revflick!, state = s_nochemo) for _ in 1:15]
-pop2 = [BacteriumBrumley(id = species[2], r = randposition(), v = randvelocity(46.5), turn! = custom_revflick!, state = s_chemo) for _ in 1:15]
+pop1 = [BacteriumBrumley(id = species[1], r = randposition(), v = randvelocity(46.5), turn! = custom_revflick!, state = copy(s_nochemo)) for _ in 1:50]
+pop2 = [BacteriumBrumley(id = species[2], r = randposition(), v = randvelocity(46.5), turn! = custom_revflick!, state = copy(s_chemo)) for _ in 1:50]
 population = vcat(pop1, pop2)
 num_bacteria = length(population)
 
 
-nsteps = 500
-save_every = 1
+nsteps = 1500
+save_every = 2
 trajectories = zeros(nsteps÷save_every, num_bacteria, d)
 
 runsim!(trajectories, nsteps, population, f; save_every=save_every)
@@ -73,15 +73,18 @@ end # function
 clims = (cfield(Lbox/3,Lbox/3), cfield(0,0))
 xx = yy = -Lbox/3:4:Lbox/3
 cc = cfield.(xx',yy)
-for t in 2:2:nsteps
+for t in 2:2:nsteps÷save_every
     p = plot(;plot_style(:Dark2)..., leg=false, axis=false, lims=(-Lbox/3,Lbox/3), aspect_ratio=1);
     heatmap!(p, xx, yy, cc, c=:bone, clims=clims)
-    x = @view trajectories[1:t,:,1]
+    ltail = max(t-20, 1)
+    x = @view trajectories[ltail:t,:,1]
     xend = @view trajectories[t,:,1]
-    y = @view trajectories[1:t,:,2]
+    y = @view trajectories[ltail:t,:,2]
     yend = @view trajectories[t,:,2]
-    plot!(p, x, y, lab=false, lw=1, linealpha=range(0.1, 1; length=t), lc=linecolor)
-    scatter!(p, xend, yend, lab=false, m=:c, msw=0, mc=linecolor|>permutedims)
+    plot!(p, x, y, lab=false, lw=0.5, lc=linecolor)#, linealpha=range(0.1, 1; length=t)
+    scatter!(p, xend, yend, lab=false, m=:c, ms=3, msw=0, mc=linecolor|>permutedims)
+    plot!([xx[8], xx[8]+100], [yy[6], yy[6]], c=:white, lab=false, lw=4)
+    annotate!(xx[8]+50, yy[6]+30, text("100 μm", :center, :white, 6))
     idx = lpad(t, 4, '0')
     savefig(p, "frame_$(idx).png")
 end # for
