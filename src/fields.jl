5f75cb50e5009bb87ea660d9e84900e5ab6cd6c2
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-04-17 13:36:10 +0200

    Simplify and generalize fields and sensing interface

--- src/fields.jl
+++ src/fields.jl
@@ -5,16 +5,15 @@ export
 
 abstract type AbstractField end
 
-struct EmptyField <: AbstractField
-    ;
-end # struct
-
 abstract type AbstractAnalyticField <: AbstractField end
 
 @with_kw struct AnalyticField <: AbstractAnalyticField
-    ϕ::Function
-    ∇ϕ::Function
+    ϕ::Function = zerofunc
+    ∇ϕ::Function = zerofunc
+    ∂ₜϕ::Function = zerofunc
 end # struct
+
+EmptyField() = AnalyticField()
 
 @with_kw struct Concentration_SteadyDiffusionSphericalSource_3D <: AbstractAnalyticField
     R::Float64 # μm, source radius
@@ -22,5 +21,16 @@ end # struct
     Cbg::Float64 = 0.0 # μM, background concentration
     ϕ::Function = (b; kwargs...) -> Cbg + (C-Cbg)*R/norm(b.r) # concentration field
     ∇ϕ::Function = (b; kwargs...) -> -C*R/(norm(b.r)^2) # concentration gradient (radial towards center)
+    ∂ₜϕ::Function = zerofunc # no time derivative
 end # struct
 
+
+#=
+abstract type AbstractNumericField <: AbstractField
+
+@with_kw struct NumericField <: AbstractNumericField
+    ϕ
+    ∇ϕ
+    ∂ₜϕ
+end # struct
+=#
--- src/sensing.jl
+++ src/sensing.jl
@@ -1,18 +1,8 @@
-export
-    sense_f!, sense_∇f!, sense_f_∇f!
+export sense!
 
-function sense_f!(bacterium::B, f::F, affect!; kwargs...) where {B<:AbstractBacterium,F<:AbstractAnalyticField}
-    ϕ = f.ϕ(bacterium; kwargs...)
-    affect!(bacterium, ϕ)
-end # function
-
-function sense_∇f!(bacterium::B, f::F, affect!; kwargs...) where {B<:AbstractBacterium,F<:AbstractAnalyticField}
-    ∇ϕ = f.∇ϕ(bacterium; kwargs...)
-    affect!(bacterium, ∇ϕ)
-end # function
-
-function sense_f_∇f!(bacterium::B, f::F, affect!; kwargs...) where {B<:AbstractBacterium,F<:AbstractAnalyticField}
+function sense!(bacterium::B, f::F, affect!; kwargs...) where {B<:AbstractBacterium,F<:AbstractAnalyticField}
     ϕ = f.ϕ(bacterium; kwargs...)
     ∇ϕ = f.∇ϕ(bacterium; kwargs...)
-    affect!(bacterium, ϕ, ∇ϕ)
+    ∂ₜϕ = f.∂ₜϕ(bacterium; kwargs...)
+    affect!(bacterium, ϕ, ∇ϕ, ∂ₜϕ; kwargs...)
 end # function
