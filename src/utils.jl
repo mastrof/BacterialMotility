3ab11034a59fdd119fbd382bbe32fa1b8fe1c43f
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-05-17 14:56:49 +0200

    Include proper type signatures for dummy functions

--- src/utils.jl
+++ src/utils.jl
@@ -3,8 +3,16 @@
 =#
 
 dummy(args...; kwargs...) = nothing
+dummy_run(b::AbstractBacterium, dt) = nothing
+dummy_turn(b::AbstractBacterium) = nothing
+dummy_sense(bs::BacterialSystem, i) = nothing
+dummy_callback_inner(bs::BacterialSystem, i::Int) = nothing
+dummy_callback_outer(bs::BacterialSystem) = nothing
+dummy_boundary_conditions(b::AbstractBacterium) = nothing
 
 zerofunc(args...; kwargs...) = 0.0
+zeroscalar(bs::BacterialSystem, i::Int) = 0.0
+zerovector(bs::BacterialSystem, i::Int) = zeros(size(first(bs.population).r))
 
 struct Degenerate{T<:Real} <: ContinuousUnivariateDistribution
     x::T
